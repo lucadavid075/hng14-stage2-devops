56f17ad6a4a3e38faa0035ce5882237e770c49d8
Luke David <daiveedlucas049@gmail.com>
2026-04-24 07:18:05 +0000

    fix: read redis connection from environment variables

--- api/main.py
+++ api/main.py
@@ -5,7 +5,10 @@ import os
 
 app = FastAPI()
 
-r = redis.Redis(host="localhost", port=6379)
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,)
 
 @app.post("/jobs")
 def create_job():
