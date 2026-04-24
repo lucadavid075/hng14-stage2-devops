41815862335da37ae573b9afb70a7fa23837f898
Luke David <daiveedlucas049@gmail.com>
2026-04-24 07:24:00 +0000

    fix: return http 404 instead of 200

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import redis
 import uuid
 import os
@@ -27,5 +27,5 @@ def create_job():
 def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
     if not status:
-        return {"error": "not found"}
+        return HTTPException(status_code=404, detail="Job not found")
     return {"job_id": job_id, "status": status.decode()}
